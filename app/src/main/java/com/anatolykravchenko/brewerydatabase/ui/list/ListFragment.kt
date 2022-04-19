f438ecea2a2fe59bf3f34653d11bca30a14d4f26
Acses92 <40656486+Acses92@users.noreply.github.com>
2022-04-19 21:46:19 +0500

    Вынес NetworkErrorExt.kt в отдельный файл

--- app/src/main/java/com/anatolykravchenko/brewerydatabase/ui/list/ListFragment.kt
+++ app/src/main/java/com/anatolykravchenko/brewerydatabase/ui/list/ListFragment.kt
@@ -14,7 +14,7 @@ import com.anatolykravchenko.brewerydatabase.ui.adapters.BreweryListAdapter
 import com.anatolykravchenko.brewerydatabase.ui.detail.BreweryDetailFragment
 import com.anatolykravchenko.brewerydatabase.util.Status
 import dagger.hilt.android.AndroidEntryPoint
-import com.anatolykravchenko.brewerydatabase.data.network.NetworkError
+import com.anatolykravchenko.brewerydatabase.util.getString
 
 
 @AndroidEntryPoint
@@ -95,14 +95,5 @@ class ListFragment : Fragment(R.layout.brewery_list_fragment) {
 
     }
 
-    private fun NetworkError.getString(): Int =
-        when(this) {
-            NetworkError.NO_CONNECTION -> R.string.network_error_no_connection
-            NetworkError.UNAUTHORIZED -> R.string.network_error_unauthorized
-            NetworkError.BAD_REQUEST -> R.string.network_error_bad_request
-            NetworkError.INTERNAL_SERVER_ERROR -> R.string.network_error_internal_server_error
-            NetworkError.NOT_FOUND -> R.string.network_error_not_found
-            NetworkError.REQUEST_TIMEOUT ->R.string.network_error_request_timeout
-            NetworkError.UNKNOWN -> R.string.network_error_unknown
-        }
+
 }
